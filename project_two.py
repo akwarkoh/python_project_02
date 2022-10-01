1873f35283cf44cf534b6f5484b1dd446916614e
akwarkoh <angelakwarko@gmail.com>
2022-09-30 20:05:39 -0700

    final edit

--- project_two.py
+++ project_two.py
@@ -4,7 +4,7 @@ import crayons
 
 def showInstructions():
     """Game intsructions for user to follow"""
-    #print a main menu and the commands
+    # print a main menu and the commands
     print(crayons.green('''
     RPG Game
     ========
@@ -19,7 +19,7 @@ def showStatus():
     print('---------------------------')
     print(crayons.green('You are in the ' + currentRoom))
     
-    #prints item the player has
+    # prints item the player has
     print(crayons.blue('Inventory:', inventory))
     # Print item in current room
     if "item" in rooms[currentRoom]:
@@ -120,7 +120,7 @@ showInstructions()
 while True:
     showStatus()
 
-    #player input
+    # player input
     move = ''
     while move == '':  
         move = input('>')
@@ -128,48 +128,48 @@ while True:
     # .lower() to convert input into lowercase and .split() to turn it into a list      
     move = move.lower().split(" ", 1)
 
-    #if they type 'go' first
+    # if they type 'go' first
     if move[0] == 'go':
         if move[1] in rooms[currentRoom]:
-            #set the current room to the new room
+            # set the current room to the new room
             currentRoom = rooms[currentRoom][move[1]]
        
         else:
             print(crayons.red('You can\'t go that way!'))
 
-    #if they type 'get' first
+    # if they type 'get' first
     if move[0] == 'get' :
         # make two checks:
         # 1. if the current room contains an item
         # 2. if the item in the room matches the item the player wishes to get
         if "item" in rooms[currentRoom] and move[1] in rooms[currentRoom]['item']:
-            #add the item to their inventory
+            # add the item to their inventory
             inventory.append(move[1])
-            #display a helpful message
+            # display a helpful message
             print(crayons.green(move[1] + ' got!'))
-            #delete the item key:value pair from the room's dictionary
+            # delete the item key:value pair from the room's dictionary
             del rooms[currentRoom]['item']
         else:
-            #tell them they can't get it
+            # tell them they can't get it
             print(crayons.red('Can\'t get ' + move[1] + '!'))
 
-     #shows how a player can lose
+     # shows how a player can lose
     if 'item' in rooms[currentRoom] and 'monster' in rooms[currentRoom]['item']:
         print(crayons.red('A monster has got you... GAME OVER!'))
         break 
 
-          #shows how a player wins
+          # shows how a player wins
     if currentRoom == 'Sun Room' and 'key' in inventory and 'gold' in inventory:
         print(crayons.red('You escaped the house with the ultra rare key and magic potion... YOU WIN!'))
         break
       
-    #shows how a player can move to any room from their current location
+    # shows how a player can move to any room from their current location
     if currentRoom == 'Tunnel' and 'teleport' in inventory:
         
-        #prints a list of all the rooms
+        # prints a list of all the rooms
         print(crayons.red(rooms.keys()))
 
-        #user input to choose a room they want to move to 
+        # user input to choose a room they want to move to 
         choice = input(crayons.green("Choose a room: "))
         currentRoom = choice
   
